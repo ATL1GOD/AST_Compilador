7e8cca667dd944b7d47fa85aff70323adab22f21
Atl Cardoso <atl1cardoso0@gmail.com>
2024-01-07 21:30:17 -0600

    Se corrijo un error en exprVariable

--- Expressions/ExprVariable.java
+++ Expressions/ExprVariable.java
@@ -3,7 +3,7 @@ package Expressions;
 import Utils.*;
 
 public class ExprVariable extends Expression {
-    final Token name;
+    public final Token name;
 
     public ExprVariable(Token name) {
         this.name = name;
