1ec61511ee3b6d2760173e41cbc30245f8d2dee6
Atl Cardoso <atl1cardoso0@gmail.com>
2024-01-07 21:32:37 -0600

    Se corrijio el error de la linea 217 en el AST.java

--- Expressions/ExprVariable.java
+++ Expressions/ExprVariable.java
@@ -2,8 +2,9 @@ package Expressions;
 
 import Utils.*;
 
-public class ExprVariable extends Expression {
-    public final Token name;
+public class ExprVariable extends Expression { 
+    public final Token name; 
+    //para corregir el error de la linea 217 del asrchivo AST.java tuve hacer publica la variable name
 
     public ExprVariable(Token name) {
         this.name = name;
