c7779708d241d900752db8cb5bf5372c64f8e4ac
MarianGuzman1829 <147306242+MarianGuzman1829@users.noreply.github.com>
2024-01-12 00:20:09 -0600

    Se añadio la funcion resolver a la clase y se importa el paquete correspondiente

--- Expressions/ExprLiteral.java
+++ Expressions/ExprLiteral.java
@@ -1,9 +1,13 @@
 package Expressions;
 //import Utils.Token
+import Utils.*;
 public class ExprLiteral extends Expression {
     final Object value;
 
     public ExprLiteral(Object value) {
         this.value = value;
+    }
+    public Object resolver (TablaSimbolos tabla){
+        return value;
     }
 }
