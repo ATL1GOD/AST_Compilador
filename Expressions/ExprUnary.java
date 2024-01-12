b8c18239fffbdb5532aa6ba858740a9136b57961
Daniel Hernandez <105814072+Dxniel7@users.noreply.github.com>
2024-01-12 03:05:56 -0600

    Se anexo el resolver a ExprUnary

--- Expressions/ExprUnary.java
+++ Expressions/ExprUnary.java
@@ -10,4 +10,26 @@ public class ExprUnary extends Expression{
         this.operator = operator;
         this.right = right;
     }
+
+    public Object resolver(TablaSimbolos tabla){
+        Object derecha = right.resolver(tabla);
+
+        switch (operator.getTipo()){
+            case BANG -> {
+                if(derecha instanceof Boolean){
+                    return !(boolean) derecha;
+                } else{
+                    throw new RuntimeException("Este operador '!' solo puede ser empleado en expresiones booleanas");
+                }
+            }
+            case MINUS -> {
+                if(derecha instanceof Integer || derecha instanceof Double){
+                    return -(double)derecha;
+                } else{
+                    throw new RuntimeException("El operador menos solo puede ser empleado en números");
+                }
+            }
+            default -> throw new RuntimeException("Operador no encontrado");
+        }
+    }
 }
