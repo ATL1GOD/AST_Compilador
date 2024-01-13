70dc5564b3e8fe1d87d458a23f7f86e53d19fc6c
MarianGuzman1829 <147306242+MarianGuzman1829@users.noreply.github.com>
2024-01-13 14:32:45 -0600

    Se arreglaron unos problemas que afectaban otras clases

--- Utils/TablaSimbolos.java
+++ Utils/TablaSimbolos.java
@@ -1,5 +1,5 @@
 package Utils;
-
+import java.util.*;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -7,18 +7,18 @@ public class TablaSimbolos {
 
     private final Map<String, Object> values = new HashMap<>();
 
-    boolean existeIdentificador(String identificador){
+    public boolean existeIdentificador(String identificador){
         return values.containsKey(identificador);
     }
 
-    Object obtener(String identificador) {
+    public Object obtener(String identificador) {
         if (values.containsKey(identificador)) {
             return values.get(identificador);
         }
         throw new RuntimeException("Variable no definida '" + identificador + "'.");
     }
 
-    void asignar(String identificador, Object valor){
+    public void asignar(String identificador, Object valor){
         values.put(identificador, valor);
     }
 
