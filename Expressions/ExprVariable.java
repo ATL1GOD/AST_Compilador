398db90dd37e1ded0871092bc4005fc2ee165f72
Daniel Hernandez <105814072+Dxniel7@users.noreply.github.com>
2024-01-15 12:47:17 -0600

    Se busco el error del compilador general y econtramos que no se creaba la variable.

--- Expressions/ExprVariable.java
+++ Expressions/ExprVariable.java
@@ -10,8 +10,17 @@ public class ExprVariable extends Expression {
         this.name = name;
     }
 
+    public String getNombre(){
+        return name.getLexema(); 
+    }
+    
+
     public Object resolver(TablaSimbolos tabla){
         if(tabla.existeIdentificador(name.getLexema())){
+            Object valor = tabla.obtener(name.getLexema());
+            if(valor == null){
+                throw new RuntimeException("Variable " + name.getLexema() + " no inicializada");
+            }
             return tabla.obtener(name.getLexema());
         }else{
             throw new RuntimeException("La variable no se encuentra definida '" + name.getLexema() + "'.");
